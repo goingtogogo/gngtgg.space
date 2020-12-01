741a0dd775641f64e378911c1491d93193631b10
Leyla <leka2101@mail.ru>
2020-12-01 17:44:18 +0300

    add dark/light mode

--- source/js/script.js
+++ source/js/script.js
@@ -2,11 +2,13 @@ import onLoad from './modules/on-load';
 import menu from './modules/menu';
 import form from './modules/form';
 import Slider from './modules/slider';
+import theme from './modules/theme';
 
 // init modules
 onLoad();
 menu();
 form();
+theme();
 
 const slider = new Slider();
 slider.init();
