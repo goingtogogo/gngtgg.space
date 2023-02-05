b35ac1e6a29d452bc225fa0c336fef56c9a24500
Лейла Мамедова <mamedovaleyla@MacBook-Air-Lejla-2.local>
2023-02-05 22:58:20 +0100

    fix: analytics init

--- source/js/script.js
+++ source/js/script.js
@@ -2,12 +2,14 @@ import onLoad from './modules/on-load';
 import menu from './modules/menu';
 import form from './modules/form';
 import Slider from './modules/slider';
+import { inject } from '@vercel/analytics';
 // import theme from './modules/theme';
 
 // init modules
 onLoad();
 menu();
 form();
+inject();
 // theme();
 
 const slider = new Slider();
